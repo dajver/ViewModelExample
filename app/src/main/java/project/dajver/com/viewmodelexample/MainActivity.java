648ecaa9ba624f77a0e1b2a54ac21e7e75f3f232
gleb <lfqdth5@gmail.com>
2018-05-23 14:32:38 +0300

    first commit

---
+++ app/src/main/java/project/dajver/com/viewmodelexample/MainActivity.java
@@ -0,0 +1,53 @@
+package project.dajver.com.viewmodelexample;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textA)
+    TextView textA;
+    @BindView(R.id.textB)
+    TextView textB;
+
+    private ScoreViewModel mViewModel;
+    private int counterA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mViewModel = ViewModelProviders.of(this).get(ScoreViewModel.class);
+        displayForTeamA(counterA);
+        displayForTeamB(mViewModel.scoreTeamB);
+    }
+
+    @OnClick(R.id.buttonA)
+    public void onButtonAClick() {
+        displayForTeamA(counterA++);
+    }
+
+    @OnClick(R.id.buttonB)
+    public void onButtonBClick() {
+        mViewModel.scoreTeamB = mViewModel.scoreTeamB + 1;
+        displayForTeamB(mViewModel.scoreTeamB);
+    }
+
+    public void displayForTeamA(int v) {
+        textA.setText(String.valueOf(v));
+    }
+
+    public void displayForTeamB(int v) {
+        textB.setText(String.valueOf(v));
+    }
+
+
+}
---
+++ app/src/main/java/project/dajver/com/viewmodelexample/ScoreViewModel.java
@@ -0,0 +1,9 @@
+package project.dajver.com.viewmodelexample;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ScoreViewModel extends ViewModel {
+
+    public int scoreTeamA = 0;
+    public int scoreTeamB = 0;
+}
